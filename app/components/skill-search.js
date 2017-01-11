22f7dc650f9f9228de9b6d5791af540d53e8003b
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-01-10 22:07:00 -0600

    Added basic typeahead support

--- app/components/skill-search.js
+++ app/components/skill-search.js
@@ -1,14 +1,61 @@
-'use strict';
-
 import React from 'react';
+import $ from 'jquery';
+import 'typeahead.js';
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
 
 class SkillSearch extends React.Component {
 
+  componentDidMount() {
+    $('.skm-skill-search').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    }, {
+      name: 'states',
+      source: substringMatcher(states)
+    });
+  }
+
   render() {
     return (
-      <button type="button" className="btn btn-default btn-lg">
-        <span className="glyphicon glyphicon-star" aria-hidden="true"></span> Star
-      </button>
+      <div>
+        <input type="text"
+          className="skm-skill-search form-control"
+          placeholder="Skill search...">
+        </input>
+      </div>
     );
   }
 
