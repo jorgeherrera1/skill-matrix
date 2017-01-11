9441239ad9e1576b080afef577a33f746d56726e
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-01-10 21:43:29 -0600

    Modified public path of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: '/assets'
+    publicPath: '/'
   },
   module: {
     rules: [
