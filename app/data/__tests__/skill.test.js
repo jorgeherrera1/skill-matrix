dc46326dd38b198d82e0e99085ee822b8bb7e24a
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-01-13 19:17:09 -0600

    Added jest for testing and graphlib

---
+++ app/data/__tests__/skill.test.js
@@ -0,0 +1,5 @@
+import Skill from '../skill';
+
+test('a skill is created', () => {
+  const javaSkill = new Skill('Java');
+});
---
+++ app/data/skill.js
@@ -0,0 +1,11 @@
+import Graph from 'graphlib';
+
+class Skill {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+export default Skill;
