c99af8104085817ed62b290e3a634123fd0e54c6
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-02-06 22:01:02 -0600

    started webpack config from scratch

---
+++ app/scripts/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+  <h3>Index</h3>,
+  document.getElementById('skill-matrix-app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  context: path.resolve(__dirname, 'app'),
+  entry: {
+    app: './scripts/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.html$/,
+        loaders: ['html-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './index.html'
+  })],
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
+
+module.exports = config;
