f52833c84c1f669d9d9b1a0737ac857973dd2e70
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-02-07 21:50:42 -0600

    refactorized search to be a component

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,34 +1,44 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 
-const Search = ({skills, onSkillSearch}) => {
-  let input;
+class Search extends Component {
 
-  const skillsSearched = skills.map((skill, idx) => (
-    <li key={idx}>{skill}</li>
-  ));
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        if (!input.value.trim()) {
-          return;
-        }
-        onSkillSearch(input.value);
-        input.value = '';
-      }}>
-        <input type="text" ref={(node) => {
-          input = node;
-        }}/>
-        <button type="submit">Search</button>
-        <br />
-        <ul>
-          {skillsSearched}
-        </ul>
-      </form>
-    </div>
-  );
-};
+    this.addSkillToSearch = this.addSkillToSearch.bind(this);
+  }
+
+  addSkillToSearch(e) {
+    e.preventDefault();
+
+    if (!this.input.value.trim()) {
+      return;
+    }
+
+    this.props.onSkillSearch(this.input.value);
+    this.input.value = '';
+  }
+
+  render() {
+    const skillsSearched = this.props.skills.map((skill, idx) => (
+      <li key={idx}>{skill}</li>
+    ));
+
+    return (
+      <div>
+        <form onSubmit={this.addSkillToSearch}>
+          <input type="text" ref={(node) => {this.input = node;}}/>
+          <button type="submit">Search</button>
+          <br />
+          <ul>
+            {skillsSearched}
+          </ul>
+        </form>
+      </div>
+    );
+  }
+
+}
 
 Search.propTypes = {
   skills: PropTypes.arrayOf(PropTypes.string).isRequired,
