4517e9821735af76ad80cdd0030188e9866b08c9
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-04-22 15:41:16 -0500

    Installed prop-types packages and updated code to use it

--- app/scripts/components/tag/tag.js
+++ app/scripts/components/tag/tag.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import '../../../styles/_tag.scss';
 
 class Tag extends Component {
