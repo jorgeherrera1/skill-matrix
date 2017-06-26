88c0a4b870d76b48dbf4e2d53a7bb295a43c2de3
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-06-25 19:05:15 -0500

    Remove all!!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
-  context: path.resolve(__dirname, 'app'),
-  entry: {
-    app: './scripts/index.js',
-    lib: [
-      'react',
-      'react-dom',
-      'prop-types',
-      'react-router-dom',
-      'react-router-redux',
-      'react-redux',
-      'history',
-      'redux',
-      'redux-logger',
-      'immutable'
-    ]
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader?minimize=true', 'sass-loader']
-        })
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'lib',
-      minChunks: Infinity
-    }),
-    new HtmlWebpackPlugin({template: './index.html'}),
-    new ExtractTextPlugin('style.[contenthash].css')
-  ],
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  }
+  
 };
 
 module.exports = config;
