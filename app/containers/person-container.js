0e8a059093c030aa3774fa3d6043dd3971eb88df
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-08-07 00:25:03 -0500

    Added chip to display skills and people

--- app/containers/person-container.js
+++ app/containers/person-container.js
@@ -4,7 +4,11 @@ const PersonContainer = () => (
   <div>
     <h1>Person</h1>
     <div className="mdl-grid">
-        <div className="mdl-cell mdl-cell--4-col">jr</div>
+        <div className="mdl-cell mdl-cell--4-col">
+          <span className="mdl-chip">
+            <span className="mdl-chip__text">Java</span>
+          </span>
+        </div>
         <div className="mdl-cell mdl-cell--4-col">int</div>
         <div className="mdl-cell mdl-cell--4-col">sr</div>
     </div>
