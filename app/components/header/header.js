19f7cadf0a7501987f0fc54946d57cea9153e380
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-08-06 23:40:42 -0500

    Basic layout and theme changes

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-  <header className="mdl-layout__header">
+  <header className="mdl-layout__header mdl-layout__header--transparent">
     <div className="mdl-layout__header-row">
       <span className="mdl-layout-title">Skill Matrix</span>
       <div className="mdl-layout-spacer"></div>
