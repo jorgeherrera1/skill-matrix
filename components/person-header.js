c78e810892823d49bb5958ebb1349386e439a103
Jorge Herrera <jorge@Jorges-MacBook-Pro.local>
2018-07-06 21:08:21 -0500

    Created person header

---
+++ components/person-header.js
@@ -0,0 +1,6 @@
+export default () => (
+  <header>
+    <h1>Jorge Herrera</h1>
+    <p>Service Delivery Director</p>
+  </header>
+);
--- pages/person.js
+++ pages/person.js
@@ -1,11 +1,10 @@
 import Page from '../layouts/main';
+import PersonHeader from '../components/person-header';
 import LevelContainer from '../components/level-container';
 
 export default () => (
   <Page>
-    <header>
-      <h1>Jorge Herrera</h1>
-    </header>
+    <PersonHeader />
     <main>
       <LevelContainer />
     </main>
